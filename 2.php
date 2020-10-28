774d2083177b294450d9e7a0db830f01e2c38d3d
Vladimir Gryanko <gryanko20012001@mail.ru>
2020-10-28 10:14:03 +0300

    add mySortForKey

--- 2.php
+++ 2.php
@@ -16,3 +16,39 @@ function convertString($a, $b)
 }
 // Вывод функции convertString
 // convertString('hello hello hello', 'el');
+function mySortForKey($a, $b)
+{
+  foreach ($a as $key => $v) {
+    if (!array_key_exists($b, $a[$key])) {
+      throw new Exception("Во вложенном массиве с индексом  $key нет ключа '$b'");
+      return 0;
+    }
+  }
+  foreach ($a as $key => $row) {
+    $volume[$key]  = $row[$b];
+  }
+  array_multisort($volume, SORT_ASC, $a);
+  foreach ($a as $key => $v) {
+    echo $key . "=";
+    var_dump($v);
+    echo "</br>";
+  }
+  echo "Отсортировано по ключу '$b' в порядке возрастания.</br>";
+}
+$array1 = array(
+  ["a" => 5, "b" => 1],
+  ["a" => 12, "b" => 8],
+  ["a" => 1, "b" => 7],
+  ["a" => 3, "b" => 10],
+  ["a" => 1, "b" => 5]
+);
+//Массив входных данных
+// Вывод функции mySortForKey
+// try{
+//   echo "1 сортировка : </br>";
+//   echo mySortForKey($array1,"a");
+//   echo "2 сортировка : </br>";
+//   echo mySortForKey($array1,"c");
+// }catch (Exception $e) {
+//   echo "Ошибка: ".$e->getMessage();
+// }
